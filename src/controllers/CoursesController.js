fbe294f4722dc76473e69388d52ad3d2bed0d7da
Thibault.M (JS Developer) <68784661+Thibault-MJS@users.noreply.github.com>
2022-01-16 22:13:44 +0100

    Adding routes for CRUD courses

---
+++ src/controllers/CoursesController.js
@@ -0,0 +1,23 @@
+class CoursesController {
+    static getCourses(req, res, next) {
+
+    }
+
+    static getCourse(req, res, next) {
+        
+    }
+
+    static createCourse(req, res, next) {
+        
+    }
+
+    static deleteCourse(req, res, next) {
+        
+    }
+
+    static editCourse(req, res, next) {
+        
+    }
+}
+
+module.exports = CoursesController;
--- src/routes/CoursesRoute.js
+++ src/routes/CoursesRoute.js
@@ -1,2 +1,11 @@
-const {Router} = require("express");
+const express = require("express");
+const router = express.Router();
+const CoursesController = require("../controllers/CoursesController");
 
+router.get("/", CoursesController.getCourses); // Récupérer tous les cours
+router.get("/:id", CoursesController.getCourse); // Récupérer un cours avec un ID particulier
+router.post("/", CoursesController.createCourse); // Créer un nouveau cours
+router.delete("/delete/:id", CoursesController.deleteCourse); // Supprimer un cours particulier
+router.put("/:id", CoursesController.editCourse); // Modifier un cours
+
+module.exports = router;
